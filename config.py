8221088e8f1c45a54b69ea4246e70ed65c33c1a1
rafsill <rafael.silnav@gmail.com>
2015-02-07 13:48:29 +0100

    Base commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+
+# default config
+class BaseConfig(object):
+    DEBUG = True
+    # shortened for readability
+    SECRET_KEY = '\xbf\xb0\x11\xb1\xcd\xf9\xba\x8bp\x0c...'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+
+
+class TestConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+
+class DevelopmentConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
---
+++ tests/test_basic.py
@@ -0,0 +1,27 @@
+# tests/test_basic.py
+
+import unittest
+
+from base import BaseTestCase
+
+
+class FlaskTestCase(BaseTestCase):
+
+    # Ensure that Flask was set up correctly
+    def test_index(self):
+        response = self.client.get('/login', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    # Ensure that main page requires user login
+    def test_main_route_requires_login(self):
+        response = self.client.get('/', follow_redirects=True)
+        self.assertIn(b'Please log in to access this page', response.data)
+
+    # Ensure that welcome page loads
+    def test_welcome_route_works_as_expected(self):
+        response = self.client.get('/welcome', follow_redirects=True)
+        self.assertIn(b'Welcome to Flask!', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
